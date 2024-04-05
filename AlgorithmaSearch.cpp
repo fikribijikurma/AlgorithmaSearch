435c8328dc66163ccad97e408bbfd64583693ec8
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-04-05 00:10:06 -0700

    masukan while

--- AlgorithmaSearch.cpp
+++ AlgorithmaSearch.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
+int arr[20]; // Array to be searched
+int n;       // Number of elements in the array
+int i;       // index of array element
 
+void input() {
+	while (true) {
+
+	}
+}
