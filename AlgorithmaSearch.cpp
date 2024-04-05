82779f439e34412017e2317fcb2fe25a4e4ef9f6
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-04-05 00:47:44 -0700

    masukan while

--- AlgorithmaSearch.cpp
+++ AlgorithmaSearch.cpp
@@ -57,7 +57,8 @@ void linearSearch()
 			cout << "\n " << item << " not found in the array\n";
 		cout << "\nNumber of comparisons: " << ctr endl;
 
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
 
-	}
-
+	} while ((ch == 'y') || (ch == 'y'));
 }
